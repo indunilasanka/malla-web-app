5b08098ceab173f3c42d9a22d12f8925f23e7863
indunilasanka <waiasanka.13@cse.mrt.ac.lk>
2018-06-30 11:57:40 +0530

    addmin hide option files

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://mallait.000webhostapp.com/admin/');
-define('HTTP_CATALOG', 'https://mallait.000webhostapp.com/');
+define('HTTP_SERVER', 'http://localhost/malla/admin/');
+define('HTTP_CATALOG', 'http://localhost/malla/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://mallait.000webhostapp.com/admin/');
-define('HTTPS_CATALOG', 'https://mallait.000webhostapp.com/');
+define('HTTPS_SERVER', 'http://localhost/malla/admin/');
+define('HTTPS_CATALOG', 'http://localhost/malla/');
 
 // DIR
-define('DIR_APPLICATION', '/storage/h7/091/1129091/public_html/admin/');
-define('DIR_SYSTEM', '/storage/h7/091/1129091/public_html/system/');
-define('DIR_IMAGE', '/storage/h7/091/1129091/public_html/image/');
-define('DIR_LANGUAGE', '/storage/h7/091/1129091/public_html/admin/language/');
-define('DIR_TEMPLATE', '/storage/h7/091/1129091/public_html/admin/view/template/');
-define('DIR_CONFIG', '/storage/h7/091/1129091/public_html/system/config/');
-define('DIR_CACHE', '/storage/h7/091/1129091/public_html/system/storage/cache/');
-define('DIR_DOWNLOAD', '/storage/h7/091/1129091/public_html/system/storage/download/');
-define('DIR_LOGS', '/storage/h7/091/1129091/public_html/system/storage/logs/');
-define('DIR_MODIFICATION', '/storage/h7/091/1129091/public_html/system/storage/modification/');
-define('DIR_UPLOAD', 'C/storage/h7/091/1129091/public_html/system/storage/upload/');
-define('DIR_CATALOG', '/storage/h7/091/1129091/public_html/catalog/');
+define('DIR_APPLICATION', 'C:/xampp/htdocs/malla/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/malla/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/malla/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/malla/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/malla/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/malla/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/malla/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/malla/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/malla/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/malla/system/storage/modification/');
+define('DIR_UPLOAD', 'CC:/xampp/htdocs/malla/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/malla/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'id1129091_malla_it');
-define('DB_PASSWORD', 'malla_it');
-define('DB_DATABASE', 'id1129091_malla_it');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'malla_it');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
